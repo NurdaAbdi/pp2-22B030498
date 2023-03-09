82364e417e7bcae0d2e3e0a1d460941a225bad3b
NurdaAbdi <n_abdimaulen@kbtu.kz>
2023-03-09 11:19:37 +0600

    tsis

---
+++ tsis6/builtin4.py
@@ -0,0 +1,7 @@
+import math
+print('Sample Input:')
+num = int(input())
+time = int(input())
+sqrt = math.sqrt(num)
+print('Sample Output:')
+print('Square root of {num} after {time} miliseconds is {sqrt}')
